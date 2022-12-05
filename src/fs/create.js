40bdcc1e885326b0d7f7058f85d5721d2d0f5cab
nos64 <manosov1984@gmail.com>
2022-12-05 16:26:33 +0400

    feat: fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import { constants } from 'fs';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const fileLink = path.resolve(__dirname, 'files', 'fresh.txt');
+  try {
+    await fs.access(fileLink, constants.F_OK);
+    console.log('FS operation failed');
+  } catch {
+    fs.writeFile(fileLink, 'I am fresh and young')
+  }
 };
 
 await create();
+
