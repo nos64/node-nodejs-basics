d6fa9e7463c0dea2b6c31193faebb614c61d5a6c
nos64 <manosov1984@gmail.com>
2022-12-05 16:56:08 +0400

    feat: fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const initialPath = path.join(__dirname, 'files');
+  const copyPath = path.join(__dirname, 'files_copy');
+  
+  try {
+    await fs.mkdir(copyPath, {recursive: false});
+    await fs.access(initialPath, constants.F_OK);
+    
+  } catch {
+    console.log('FS operation failed')
+  }
+  const copyFiles = await fs.readdir(initialPath);
+    for (const file of copyFiles) {
+      await fs.copyFile(
+        path.join(initialPath, file),
+        path.join(copyPath, file)
+      );
+    }
+
 };
 
 copy();
